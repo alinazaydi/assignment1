9bf9bebc0bafe0e7d92220b65216e3a573050ab6
alinazaydi <zaidialina123@gmail.com>
2025-11-07 01:25:27 +0530

    Keep only assigment1 folder

---
+++ assigment1/task_1.py
@@ -0,0 +1,11 @@
+n1 = int(input("Enter the first number: "))
+n2 = int(input("Enter the second number: "))
+Addition = n1 + n2
+Subtraction = n1 - n2
+Multiplacation = n1 * n2
+Division = n1 / n2
+print(" ")
+print("Addition:", Addition)
+print("Subtraction:", Subtraction)
+print("Multiplication:", Multiplacation)
+print("Division:", Division)
---
+++ assigment1/task_2.py
@@ -0,0 +1,4 @@
+f = input("Enter your first name: ")
+l = input("Enter your last name: ")
+
+print("Hello, " + f + " " + l + "! Welcome to the python program.")
